5085e346f8383b45a5a881bdb3a65728b48ac3e1
Joel Bodenmann <jbo@insane.engineer>
2024-04-26 03:20:50 +0200

    example: Update to use Botan 3.4.10

--- example/simple/main.cpp
+++ example/simple/main.cpp
@@ -8,12 +8,12 @@ main()
     std::string input_string = "1234";
 
     // SHA3-256
-    Botan::SHA_3 sha3_256(256);
-    std::cout << "SHA3-256: " << Botan::hex_encode(sha3_256.process(input_string)) << std::endl;
+    auto sha3_256 = Botan::HashFunction::create_or_throw("SHA-3(256)");
+    std::cout << "SHA3-256: " << Botan::hex_encode(sha3_256->process(input_string)) << std::endl;
 
     // SHA3-512
-    Botan::SHA_3 sha3_512(512);
-    std::cout << "SHA3-512: " << Botan::hex_encode(sha3_512.process(input_string)) << std::endl;
+    auto sha3_512 = Botan::HashFunction::create_or_throw("SHA-3(512)");
+    std::cout << "SHA3-512: " << Botan::hex_encode(sha3_512->process(input_string)) << std::endl;
 
     return 0;
 }
